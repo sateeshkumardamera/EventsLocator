beda81167eb9c39a0a0b57bd21b03e93878773ff
Sateesh Damera <sateesh.damera@openedgepay.com>
2021-07-12 15:44:43 -0600

    Initial Commit

---
+++ EventLocator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EventLocator
+//
+//  Created by Sateesh Damera on 7/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
